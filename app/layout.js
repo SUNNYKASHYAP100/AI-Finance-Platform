6ba3460cea35d8a309b38b18c7514ca2d862031d
SUNNYKASHYAP100 <youremail@example.com>
2025-12-23 14:13:58 +0530

    features added

---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
+
+const inter = Inter({ subsets: ["latin"]});
+
+export const metadata = {
+  title: "QuantEdge",
+  description: "AI finance platform with smart insights and strategies.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <Toaster richColors />
+          {/* richColors to automatically deduct the colors for warning , errors etc */}
+          {/* footer */}
+        <footer className="bg-blue-50 py-6">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Design & Developed by Aman Singh. All rights reserved.</p>
+            </div>
+          </footer>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
