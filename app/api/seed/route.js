d987dcecc4c2453e314988d91e93a00a633ebde4
SUNNYKASHYAP100 <youremail@example.com>
2025-12-27 04:22:47 +0530

    Refactor route and middleware to integrate arcjet for enhanced bot detection and security

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -1,6 +1,27 @@
 import { seedTransactions } from "@/actions/seed";
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+
+  rules: [
+
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+        "GO_HTTP", 
+      ],
+    }),
+  ],
+});
 
 export async function GET() {
   const result = await seedTransactions();
   return Response.json(result);
 }
+
--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+// import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -8,23 +8,22 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY,
 
-  rules: [
-
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", 
-        "GO_HTTP", 
-      ],
-    }),
-  ],
-});
+//   rules: [
+//     shield({
+//       mode: "LIVE",
+//     }),
+//     detectBot({
+//       mode: "LIVE",
+//       allow: [
+//         "CATEGORY:SEARCH_ENGINE",
+//         "GO_HTTP",
+//       ],
+//     }),
+//   ],
+// });
 
 const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
@@ -37,7 +36,9 @@ const clerk = clerkMiddleware(async (auth, req) => {
   return NextResponse.next();
 });
 
-export default createMiddleware(aj, clerk);
+// export default createMiddleware(aj, clerk);
+export default clerk;
+
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
